dc869898a76e03bbfead2a5d06a2095b1ee7ed5f
Mubashir Rasool Razvi <contact@mrrazvi.com>
2026-01-01 12:30:52 +0500

    semantic search database ingestion

--- notebooks/semantic-search/config.py
+++ notebooks/semantic-search/config.py
@@ -1,5 +1,5 @@
 QDRANT_URL = "http://localhost:6333"
-COLLECTION_NAME = "qr_en_tr_abdullah_yusuf_ali"
+COLLECTION_NAME = "qr_en_tr__abdullah-yusuf-ali"
 
 EMBEDDING_MODEL = "intfloat/multilingual-e5-large"
 VECTOR_SIZE = 1024
